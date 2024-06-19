39fd339c12de4cd5860d673453851d967232ba00
orgrinrt <ort@hiisi.digital>
2024-06-19 09:07:50 +0300

    run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 //! This crate provides the `include_proc_macro` macro.
 //!
-//! The `include_proc_macro` macro is designed with a single, focused purpose: to aid in integrating 
-//! with external tooling, particularly when working with procedural macros. 
+//! The `include_proc_macro` macro is designed with a single, focused purpose: to aid in integrating
+//! with external tooling, particularly when working with procedural macros.
 //!
 //! This macro checks if debug assertions are enabled (`#[cfg(debug_assertions)]`).
-//! If so, it includes a targeted .rs file from the Cargo project's root directory 
+//! If so, it includes a targeted .rs file from the Cargo project's root directory
 //! (obtained through the `CARGO_MANIFEST_DIR` environment variable) in the module tree.
 //!
 //! ## Example
 //! ```rust
 //! include_proc_macro::include_proc_macro!("sample");
 //! ```
-//! The command above includes `sample.rs` from the root of the Cargo project during a debug 
+//! The command above includes `sample.rs` from the root of the Cargo project during a debug
 //! assertion (development time).
 //!
 //! The macro's key parameter:
@@ -22,30 +22,32 @@
 //! For more details, please see the macro definition inside the crate.
 
 #[macro_export]
-/// `include_proc_macro` is a macro with single, focused purpose: help integrating with external 
-/// tooling in certain situations, when working with procedural macros specifically. It takes a 
+/// `include_proc_macro` is a macro with single, focused purpose: help integrating with external
+/// tooling in certain situations, when working with procedural macros specifically. It takes a
 /// single parameter:
-/// - `$file_name`: a string literal that represents the name of the proc macro source file (.rs) 
+/// - `$file_name`: a string literal that represents the name of the proc macro source file (.rs)
 /// to be
 /// included in the source tree during dev time (to enable certain more advanced IDE features).
 ///
-/// This macro checks if debug assertions are enabled (`#[cfg(debug_assertions)]`), 
-/// if so, it includes the targeted .rs file from the Cargo project's root directory 
+/// This macro checks if debug assertions are enabled (`#[cfg(debug_assertions)]`),
+/// if so, it includes the targeted .rs file from the Cargo project's root directory
 /// (obtained from `CARGO_MANIFEST_DIR` environment variable) in the module tree.
 ///
 /// # Examples
 /// ```
 /// include_proc_macro::include_proc_macro!("sample");
 /// ```
-/// The above line includes `sample.rs` from the root of the Cargo project during a debug 
+/// The above line includes `sample.rs` from the root of the Cargo project during a debug
 /// assertion (i.e dev time).
 
 macro_rules! include_proc_macro {
     ($file_name:expr) => {
         #[cfg(debug_assertions)]
-        include!(concat!(env!(
-            "CARGO_MANIFEST_DIR"), "/", 
-        $file_name, if $file_name.ends_with(".rs") { "" } else { ".rs" }
-        ,));
+        include!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/",
+            $file_name,
+            if $file_name.ends_with(".rs") { "" } else { ".rs" },
+        ));
     };
 }
